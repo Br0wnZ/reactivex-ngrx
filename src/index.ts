ac4dbee408fb9601fdd6aa90aa929f496d0ce37e
Javier Moreno Valle <jmoreno.valle@atsistemas.com>
2021-01-20 16:15:43 +0100

    Observer & subscriber

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
+import { Observable, Observer } from "rxjs";
 
+const observer: Observer<any> = {
+  next: value => console.log('next [next]: ', value),
+  error: error => console.warn('error [obs]', error),
+  complete: () => console.info('Completed [obs]')
+}
 
-console.log('Hello World!');
+// const obs$ = Observable.create() // Deprecated
 
+const obs$ = new Observable<string>( subs => {
+  subs.next('Hello')
+  subs.next('World')
+  subs.next('Hello')
+  subs.next('World')
+
+  // Error forced
+  // const a = undefined
+  // a.name = 'Javi Moreno'
+
+  subs.complete()
+  subs.next('World')
+})
+
+// obs$.subscribe( console.log )
+
+// obs$.subscribe( 
+//   value => console.log('next: ',value),
+//   error => console.warn('error', error),
+//   () => console.info('Completed')
+// )
+
+obs$.subscribe( observer )
 
 
 
